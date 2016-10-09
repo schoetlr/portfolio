86b2aaba898d9e42b6f9eb89e359d2043c4dd49e
schoetlr <srlrs20020@gmail.com>
2016-10-09 10:19:02 -0400

    colors in place w some jquery for the email label

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
    
    $('.nav-links').localScroll({duration: 500});
+
+
+   $("#envelope").hover(function(event){
+    
+     $("#email-label").fadeIn(300);     
+   }, function(event){
+     $("#email-label").fadeOut(300);
+   })
 });
